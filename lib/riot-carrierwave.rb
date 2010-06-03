bae92c8bf6d2358af7e338283bcb334a82cc4b82
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-03 15:56:53 -0700

    clean up structure

---
+++ lib/riot-carrierwave.rb
@@ -0,0 +1,4 @@
+require 'riot'
+require 'carrierwave'
+
+Dir[File.dirname(__FILE__) + '/riot-carrierwave/*.rb'].each {|file| require file }
--- test/teststrap.rb
+++ test/teststrap.rb
@@ -1,3 +1,15 @@
 require 'rubygems'
 require 'riot'
-require 'riot-carrierwave'
+require File.join(File.dirname(__FILE__),'..','lib','riot-carrierwave')
+
+
+class Uploader < CarrierWave::Uploader::Base
+  permissions 0777
+end
+
+class Riot::Situation
+
+  def fixture(image)
+    File.open File.join(File.dirname(__FILE__), 'fixtures', image)
+  end
+end
