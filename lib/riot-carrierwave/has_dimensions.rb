2d82e78cfdc149adccdf0b7a60dc619551cb446a
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-06-03 17:04:32 -0700

    buffer has_dimensions macro

--- lib/riot-carrierwave/has_dimensions.rb
+++ lib/riot-carrierwave/has_dimensions.rb
@@ -2,13 +2,31 @@ module RiotCarrierwave
   class HasDimensionsAssertion < Riot::AssertionMacro
     register :has_dimensions
 
-    def evaluate(model, width, height)
+    def evaluate(model, type, width, height)
       image = RiotCarrierwave::ImageLoader.load_image(model.current_path)
-      if image and (width == image.width and height == image.height)
-        pass "#{model.class.name} has dimensions width #{width}px and height #{height}px"
+      if image and compare(type, image, width, height)
+        pass "#{model.class.name} has dimensions #{type} width #{width}px and height #{height}px"
       else
-        fail "expected #{model.class.name} to have dimensions width #{width}px and height #{height}px"
+        fail "expected #{model.class.name} to have dimensions #{type} width #{width}px and height #{height}px"
       end
     end
+    
+    def compare(type, image, width,height)
+      case type
+      when :no_larger_than
+        width >= image.width and height >= image.height
+      when :no_smaller_than
+        width <= image.width and height <= image.height
+      when :larger_than
+        width < image.width and height < image.height
+      when :smaller_than
+        width > image.width and height > image.height
+      when :equal
+        width == image.width and height == image.height
+      else
+        false
+      end
+    end
+    
   end
 end
--- test/has_dimensions_test.rb
+++ test/has_dimensions_test.rb
@@ -8,16 +8,32 @@ context "has_dimensions macro" do
     @uploader
   end
     
-  asserts "passes when the dimensions matches"  do 
-    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, 600,396).first
+  asserts "passes when the dimensions equal matches"  do 
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :equal, 600,396).first
+  end.equals :pass
+  
+  asserts "passes when the dimensions no larger than matches"  do 
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :no_larger_than, 600,396).first
+  end.equals :pass
+  
+  asserts "passes when the dimensions no lesser than matches"  do 
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :no_smaller_than, 600,396).first
+  end.equals :pass
+  
+  asserts "passes when the dimensions larger than matches"  do 
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :larger_than, 500,296).first
+  end.equals :pass
+  
+  asserts "passes when the dimensions lesser than matches"  do 
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :no_smaller_than, 600,396).first
   end.equals :pass
   
   asserts "returns a helpful message" do 
-    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, 600,396).last
-  end.matches %r{Uploader has dimensions width 600px and height 396px}
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :equal, 600,396).last
+  end.matches %r{Uploader has dimensions equal width 600px and height 396px}
   
   asserts "fails when the dimensions do not match" do 
-    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, 12,13).first
+    RiotCarrierwave::HasDimensionsAssertion.new.evaluate(topic, :equal, 12,13).first
   end.equals :fail
   
 end
